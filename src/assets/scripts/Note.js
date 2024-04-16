69afe2201e9b9e666f3171c80a7cb00cbcb2d2de
Antoine Caps <147519666+Aetherum97@users.noreply.github.com>
2024-04-16 09:08:17 +0200

    Todolist

--- src/assets/scripts/Note.js
+++ src/assets/scripts/Note.js
@@ -4,4 +4,22 @@ export default class Note {
     this.text = text;
     this.status = status || "";
   }
+
+  render() {
+    let html = `
+      <div class="post-it ${this.status}" id="note-${this.id}">
+        <div class="text" contenteditable="true"> ${this.text}</div><div class="rename-post">
+        <div class="shuffle-icon">
+        <i class="fa-solid fa-arrows-up-down-left-right" id="shuffle"></i>
+      </div>
+    <div>
+      <span class="check-icon" id="checkBtn"><i class="fa-solid fa-check"></i></span>
+      <span><i class="fa-solid fa-x" id="remove-check"></i></span>
+      <span><i class="fa-solid fa-trash" id="remove-post"></i></span>
+    </div>
+  </div>
+  </div>
+    </div>
+    `;
+  }
 }
